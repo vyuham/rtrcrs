bf1f543ceae6b27fc58a670c4007c8f09fcf45ae
Jery Jacob Arangath <jerryjacobarangath@gmail.com>
2021-04-26 14:41:53 +0530

    Added Operator Overloading. - AddAssign \`+=\` - SubAssign \`-=\` - MulAssign \`*=\` - DivAssign \`/=\` - Replaced length_squared() internal code with a dot() - Cargo Fmt'd the code

--- src/ray.rs
+++ src/ray.rs
@@ -1,3 +1,4 @@
+use crate::Color;
 use crate::Vec3;
 pub type Point3 = Vec3;
 
@@ -11,7 +12,19 @@ impl Ray {
         Self { orig, dir }
     }
 
-    pub fn origin(self) -> Point3 { self.orig }
-    pub fn direction(self) -> Vec3 { self.dir }
-    pub fn at(self, t: f64) -> Vec3 { self.orig + self.dir*t }
+    pub fn origin(self) -> Point3 {
+        self.orig
+    }
+    pub fn direction(self) -> Vec3 {
+        self.dir
+    }
+    pub fn at(self, t: f64) -> Vec3 {
+        self.orig + self.dir * t
+    }
+
+    pub fn color(self) -> Color {
+        let unit_dir = Vec3::unit_vector(self.dir);
+        let t = 0.5 * (unit_dir.y() + 1.0);
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
 }
